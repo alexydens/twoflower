ef5f3aafb0304607bc5ab11de91eebc2db2a629b
alexydens <alexydens@outlook.com>
2024-04-03 14:22:50 +0100

    Reset test/main.c

--- test/main.c
+++ test/main.c
@@ -1,11 +1,11 @@
 /* Twoflower headers */
 #include <base.h>
-#include <6502.h>
 
 /* libc headers */
 #include <stdio.h>
 
 /* Entry point */
 int main(void) {
+  printf("Hello world\n");
   return 0;
 }
